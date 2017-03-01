b2d0859b0c27fece6f82300b6d388881aeaa8efe
georoot <rb1223334444@gmail.com>
2017-03-01 15:48:49 +0530

    Helper functions added

---
+++ helper/helper.js
@@ -0,0 +1,68 @@
+const user = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.authSecret;
+const rounds = process.env.hashRounds;
+
+var helper = {};
+
+helper.hashPassword = (body)=>{
+  return new Promise((fullfill,reject)=>{
+    bcrypt.hash(body.password,rounds,(err,hash)=>{
+      if(err) reject(err);
+      else{
+        body.password = hash;
+        fullfill(body);
+      }
+    });
+  });
+}
+
+helper.signup = (body)=>{
+  return new Promise((fullfill,reject)=>{
+    new user(body)
+      .save()
+      .then(fullfill)
+      .catch(reject);
+  });
+}
+
+helper.getUser = (body)=>{
+  return new Promise((fullfill,reject)=>{
+    user
+      .findOne({email: body.email})
+      .then((user)=>{
+        if(user == null)
+          reject(new Error("No user found"));
+        else{
+          body.user = user;
+          fullfill(body);
+        }
+      })
+      .catch(reject);
+  });
+}
+
+helper.validatePassword = (body)=>{
+  return new Promise((fullfill,reject)=>{
+    bcrypt.compare(body.password,body.user.password,(err,res)=>{
+      if(err || res == false) reject(new Error("Invalid credentials"));
+      else fullfill(body.user.toObject());
+    }
+  });
+}
+
+helper.genToken = (user)=>{
+  return new Promise((fullfill,reject)=>{
+    try{
+      var token = jwt.encode(user,secret);
+      var resp = {key: token,role: user.role};
+      fullfill(resp);
+    }catch(ex){
+      reject(ex);
+    }
+  });
+}
+
+module.exports = helper;
