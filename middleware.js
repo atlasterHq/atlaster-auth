211909588fea1ac2811cae9400d8547721c2ebbb
georoot <rb1223334444@gmail.com>
2017-03-06 07:40:43 +0530

    WIP

--- middleware.js
+++ middleware.js
@@ -1,13 +1,13 @@
-const jwt = require('jsonwebtoken');
-const response = require('response');
-const secret = process.env.authSecret;
+var jwt = require('jsonwebtoken');
+var response = require('response');
+var secret = process.env.authSecret;
 
 var helper = {};
 
-helper.getUser = (req,res,next)=>{
+helper.getUser = function(req,res,next){
   var token = req.header.authorization;
   if(token){
-    jwt.verify(token,secret,(err,decoded)=>{
+    jwt.verify(token,secret,function(err,decoded){
       if(err){
         req.authenticated = false;
         next();
@@ -22,26 +22,28 @@ helper.getUser = (req,res,next)=>{
     req.authenticated = false;
     next();
   }
-}
+};
 
-helper.allowIfAuthenticated = (req,res,next)=>{
+helper.allowIfAuthenticated = function(req,res,next){
   var respCtx = new response(req,res);
-  if(req.authenticated)
+  if(req.authenticated){
     next();
+  }
   else{
     respCtx.customResponse(401,"Unauthorized",null);
   }
-}
+};
 
 helper.allowByRole = function(roleList){
-  return (req,res,next)=>{
+  return function(req,res,next){
     var respCtx = new response(req,res);
-    if(roleList.indexOf(req.user.role) > -1)
+    if(roleList.indexOf(req.user.role) > -1){
       next();
+    }
     else{
       respCtx.customResponse(401,"Unauthorized",null);
     }
-  }
-}
+  };
+};
 
 module.exports = helper;
